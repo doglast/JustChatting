dbd1d84aa74d4c1e15c087a3b4b6a697ef3eb900
Doglast <des212425@gmail.com>
2022-02-03 11:38:54 -0300

    Chat engine config

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
+import { ChatEngine } from 'react-chat-engine';
 import './App.css';
+import ChatFeed from './components/ChatFeed';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () =>{
+  return(
+    <ChatEngine
+      height="100vh"
+      projectID="559b2826-3b56-410b-95db-f2b531794675"
+      userName="DogLast"
+      userSecret="des212425"
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps}/>}
+    />
+  )
 }
 
 export default App;
